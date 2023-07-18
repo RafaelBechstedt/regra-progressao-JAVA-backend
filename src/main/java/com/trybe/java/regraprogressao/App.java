a7b7784bcfbf904ccade760de21ebbd156231631
Rafael <bechstedt@hotmail.com>
2023-07-18 19:00:52 -0300

    Req 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,26 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
 
-  /** Boraaaa
-   * Metodo main.
+
+  /**
+   * Boraaaa Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int numeroDeAtividades = scanner.nextInt();
+
+    String[] atividades = new String[numeroDeAtividades];
+
+    for (int i = 0; i < numeroDeAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      atividades[i] = scanner.next();
+    }
   }
 }
