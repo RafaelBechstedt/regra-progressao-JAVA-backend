d19217b563c3f2fc2302fd6129b174fa4d8a7559
Rafael <bechstedt@hotmail.com>
2023-07-18 19:22:26 -0300

    Req 01 - Tinha esquecido da parte do peso das atividades

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -15,12 +15,18 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int numeroDeAtividades = scanner.nextInt();
+    scanner.nextLine();
 
     String[] atividades = new String[numeroDeAtividades];
+    int[] pesosAtividades = new int[numeroDeAtividades];
 
     for (int i = 0; i < numeroDeAtividades; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
-      atividades[i] = scanner.next();
+      atividades[i] = scanner.nextLine();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      pesosAtividades[i] = scanner.nextInt();
+      scanner.nextLine();
     }
   }
 }
